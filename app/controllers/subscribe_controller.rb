738a33a197b73700c9ab068faf74f2c6dd79bbea
ShiriNmi1520 <s20026352@gmail.com>
2022-12-12 20:45:23 +0800

    wip: Subscribe controller Try implement request body validation using json-schema

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -1,13 +1,33 @@
+require 'json-schema'
 class SubscribeController < ApplicationController
   def create
+    @subscribe = Subscribe.new(create_subscribe_body)
   end
 
-  def update
+  def update; end
+
+  def get
+    param! :id, String, required: true
+    param! :station_name, String
+    param! :bus_number, String
   end
 
-  def list
-  end
+  def delete; end
 
-  def delete
+  private
+
+  def post_body
+    body_schema = {
+      'type' => 'object',
+      'required' => %w[bus_number station direction minutes_before],
+      'properties' => {
+        'bus_number' => { 'type' => 'string' },
+        'station' => { 'type' => 'string' },
+        'direction' => { 'type' => 'boolean' },
+        'minutes_before' => { 'type' => 'integer' }
+      },
+      'additionalProperties' => true
+    }
+    JSON.validate(body_schema, request.raw_post)
   end
 end
