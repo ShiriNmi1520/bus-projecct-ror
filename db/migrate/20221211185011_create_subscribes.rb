98e57d47d7e7fd304e33d6290dc62174da5817b9
ShiriNmi1520 <s20026352@gmail.com>
2022-12-12 13:09:31 +0800

    feat: Rails migration Add migration for subscribe table

--- db/migrate/20221211185011_create_subscribes.rb
+++ db/migrate/20221211185011_create_subscribes.rb
@@ -1,8 +1,14 @@
 class CreateSubscribes < ActiveRecord::Migration[7.0]
   def change
     create_table :subscribes do |t|
-
+      t.string :subscribe_id
+      t.string :email
+      t.string :client_id
+      t.string :station_name
+      t.integer :minutes_before
       t.timestamps
+      # Foreign key for bus info
+      add_reference :buses, :bus, index: true
     end
   end
 end
