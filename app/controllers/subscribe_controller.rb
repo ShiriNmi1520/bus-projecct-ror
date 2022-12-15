bb7a6c65a3409f14d5d5c6ecd8b2917bd987e122
ShiriNmi1520 <s20026352@gmail.com>
2022-12-15 13:33:42 +0800

    wip: Update subscribe Working on updating exist subscribe Add comments to existing code

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -3,16 +3,27 @@ require 'json'
 class SubscribeController < ApplicationController
   respond_to? :js
   def create
-    @validation_result = validate_post_body
-    if @validation_result.as_json['status'] != 200
-      render @validation_result
+
+    # Validate request body
+    _validation_result = validate_post_body
+    if _validation_result.as_json['status'] != 200
+      render _validation_result
     else
-      @subscribe_result = Subscribe.new(request.raw_post)
+      # Create new subscribe if validate passed
+      _subscribe_result = Subscribe.new(request.raw_post)
       render json: { 'msg': 'SUBSCRIBE_CREATED' }
     end
   end
 
-  def update; end
+  def update
+    _validation_result = validate_update_body
+    if _validation_result.as_json['status'] != 200
+      render _validation_result
+    else
+      _update_subscribe_result = Subscribe.update(request.raw_post)
+      render json: { 'msg': 'SUBSCRIBE_UPDATED' }
+    end
+  end
 
   def get
     param! :id, String, required: true
@@ -25,6 +36,7 @@ class SubscribeController < ApplicationController
   private
 
   def validate_post_body
+    # Schema for request body
     body_schema = {
       'type' => 'object',
       'required' => %w[bus_number station direction minutes_before],
@@ -37,14 +49,45 @@ class SubscribeController < ApplicationController
       'additionalProperties' => true
     }
     begin
+      # Start validate process
+      # If validate passed, return request body with 200 status code
+      # Otherwise, return JSON_VALIDATION_FAILED message with 400 status code
       if JSON::Validator.validate(body_schema, request.raw_post)
         { json: request.raw_post, status: 200 }
       else
         { json: { 'err': 'JSON_VALIDATION_FAILED' }, status: 400 }
       end
+      # If any exception occurred, return 500 status code
     rescue JSON::Schema::ValidationError => e
       e.message
       { json: { 'err': 'JSON_VALIDATION_ERROR' }, status: 500 }
+    end
+  end
+
+  def validate_update_body
+    body_schema = {
+      'type' => 'object',
+      'required' => %w[],
+      'properties' => {
+        'subscribe_id' => { 'type' => 'uuid' },
+        'email' => { 'type' => 'string' },
+        'client_id' => { 'type' => 'string' },
+        'bus_number' => { 'type' => 'string' },
+        'station' => { 'type' => 'string' },
+        'direction' => { 'type' => 'boolean' },
+        'minutes_before' => { 'type' => 'integer' }
+      },
+      'additionalProperties' => false
+    }
+    begin
+      if JSON::Validator.validate!(body_schema, rqeuest.raw_post)
+        { json: request.raw_post, status: 200 }
+      else
+        { json: { 'err': 'JSON_VALIDATION_FAILED'}, status: 400 }
+      end
+    rescue JSON::Schema::ValidationError => e
+      e.message
+      { json: { 'err': 'JSON_VALIDATION_ERROR' }, status: 500}
     end
   end
 end
