5ff0da8f601b774e01b10c680473920067dc0640
ShiriNmi1520 <s20026352@gmail.com>
2022-12-15 13:33:00 +0800

    feat: Create subscribe Implement create subscribe ActiveRecord feature

--- app/models/subscribe.rb
+++ app/models/subscribe.rb
@@ -1,2 +1,10 @@
+require 'securerandom'
 class Subscribe < ApplicationRecord
+  def new(request)
+    subscribe_id = SecureRandom.uuid
+    Subscribe.create!(request.raw_post).update_column(:id, subscribe_id)
+    { json: { msg: 'SUBSCRIBED_CREATED', id: subscribe_id }, status: 200 }
+  end
+
+  def update(_request) ;end
 end
